1fc83746d396048276fb0296823a917d49c4aa97
yipeng <huang.ypeng@gmail.com>
2015-10-12 08:08:13 +0200

    init model rendering

--- src/com/yipeng/cplexj/Main.java
+++ src/com/yipeng/cplexj/Main.java
@@ -9,7 +9,9 @@ import ilog.cplex.*;
 public class Main {
 
     public static void main(String[] args) {
-        readTestGraph();
+        Graph graph = new Graph();
+        graph.read("res/dat/testcase1.dat");
+        solveODS(graph);
     }
 
     public static void readTestGraph() {
@@ -29,10 +31,45 @@ public class Main {
         Utils.console(graph.getBlockages());
     }
 
-    public static void solveODS() {
+    public static void solveODS(Graph graph) {
+        int n = graph.getNbVertices();
+        int m = graph.getNbArcs();
         try {
             IloCplex cplex = new IloCplex();
 
+            // Decision variables
+            IloNumVar[][][][] f = new IloNumVar[n][n][n][n];
+            IloNumVar[][][][] y = new IloNumVar[n][n][n][n];
+            IloNumVar[][] x = new IloNumVar[n][n];
+            // Instantiate variables
+            for (int o = 0; o < n; o++) {
+                for (int d = 0; d < n; d++) {
+                    if (o == d) continue;
+
+                    for (int i = 0; i < n; i++) {
+                        f[o][d][i] = cplex.numVarArray(n, -1, 1);
+                        y[o][d][i] = cplex.numVarArray(n, 0, 1);
+                        x[i] = cplex.numVarArray(n, 0, 1);
+                    }
+                }
+            }
+
+            // Objective
+            IloLinearNumExpr expr = cplex.linearNumExpr();
+            for (int i = 0; i < m; i++) {
+                Arc arc = graph.getArcs().get(i);
+
+                for (int o = 0; o < n; o++) {
+                    for (int d = 0; d < n; d++) {
+                        if (o == d) continue;
+
+                        expr.addTerm(arc.getCost(), y[o][d][arc.getSource()][arc.getTarget()]);
+                    }
+                }
+            }
+            cplex.addMinimize(expr);
+
+            cplex.end();
         } catch(IloException e) {
             System.err.println("Concert exception caught: " + e);
         }
